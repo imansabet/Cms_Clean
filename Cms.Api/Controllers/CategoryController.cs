49f0ac2f53237c92a81c78169965363d14b1d106
iman <dev.imansabet@gmail.com>
2024-03-01 15:24:45 +0330

    Create Web Api Project : Add Controllers

---
+++ Cms.Api/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cms.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        public CategoryController( )
+        {
+        }
+        
+    }
+}
