36c145facd6003408412c5357c1d41ed872f2652
iman <dev.imansabet@gmail.com>
2024-03-01 09:04:48 +0330

    Core Layer : Define Domain Entites

---
+++ Cms.Core/Domain/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Domain
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int PostId { get; set; }
+        public Post post { get; set; }
+
+    }
+}
