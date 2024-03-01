11e22b70db5afc1b13cbd19f12b74914162ab6ed
iman <dev.imansabet@gmail.com>
2024-03-01 10:36:21 +0330

    CoreLayer : Add interface : Repository: ICategoryRepository IPostRepository

---
+++ Cms.Core/Interfaces/Repository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Cms.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Interfaces.Repository
+{
+    public interface ICategoryRepository
+    {
+        Category GetById(int id);
+        List<Category> GetAll();
+        int Add(Category category);
+        Category Edit(Category category);
+        void Delete(int id);
+    }
+}
---
+++ Cms.Core/Interfaces/Repository/IPostRepositorty.cs
@@ -0,0 +1,14 @@
+﻿using Cms.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Core.Interfaces.Repository
+{
+    public interface IPostRepositorty
+    {
+        List<Post> GetLatestPosts (int count);
+    }
+}
